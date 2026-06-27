156b522f6c9e59bd9a8fc49d5f690937ca2de733
nat <box240308@yandex.ru>
2026-06-26 22:59:26 -0600

    Got rid of hard coded number of pages

--- data_scraper/skaters_scraper.py
+++ data_scraper/skaters_scraper.py
@@ -1,5 +1,3 @@
-from urllib import response
-
 import requests
 import pandas as pd
 import time
@@ -8,10 +6,12 @@ page_size = 100
 
 def scrape_nhl_skaters(type):
     all_players = []
+    page = 0
+    have_data = True
 
-    for page in range(0, 79):
+    while have_data:
         while True:
-            print(f"Scraping page {page + 1}")
+            print(f"{type} page {page + 1}")
             url = f"https://api.nhle.com/stats/rest/en/skater/{type}?isAggregate=true&isGame=false&start={page * page_size}&limit={page_size}&cayenneExp=gameTypeId=2%20and%20seasonId%3C=20252026%20and%20seasonId%3E=19171918"
             response = requests.get(url)
             print(f"response code: {response.status_code}")
@@ -23,8 +23,11 @@ def scrape_nhl_skaters(type):
         data = response.json()
         all_players.extend(data["data"])
 
-        df = pd.json_normalize(all_players)
-        df.to_csv(f"data/skaters_{type}.csv", index=False)
+        have_data = len(data["data"]) > 0
+        page += 1
+
+    df = pd.json_normalize(all_players)
+    df.to_csv(f"data/skaters_{type}.csv", index=False)
 
 scrape_nhl_skaters("summary")
 scrape_nhl_skaters("bios")
